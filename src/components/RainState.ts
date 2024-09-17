84bef2722f621221a544281c8d4ef6a0ab2ab079
unknown <jyfan16@hotmail.com>
2024-09-17 18:53:35 -0400

    rainstate

---
+++ src/components/RainState.ts
@@ -0,0 +1,43 @@
+export interface RainEffectState {
+    width: number
+    height: number
+    particles: ParticleState[]
+}
+
+export interface ParticleState {
+    x: number;
+    y: number;
+    l: number;
+    xs: number;
+    ys: number;
+}
+
+export function createRainState (options: { width: number, height: number, count: number }): RainEffectState {
+    let width = options.width
+    let height = options.height
+
+    return {
+        width,
+        height,
+        particles: createParticles(width, height, options.count),
+    }
+
+}
+
+export function createParticles(
+    width: number,
+    height: number,
+    count: number,
+): ParticleState[] {
+    let particles: ParticleState[] = []
+    for (let i = 0; i < count; i++) {
+        particles.push({
+            x: Math.random() * width,
+            y: Math.random() * height,
+            l: Math.random(),
+            xs: -4 + Math.random() * 4 + 2,
+            ys: Math.random() * 10 + 10,
+        });
+    }
+    return particles
+}
