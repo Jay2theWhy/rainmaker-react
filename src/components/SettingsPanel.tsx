8feffd16ffe8bf77edde05216bbd7c9217c16ae4
unknown <jyfan16@hotmail.com>
2024-09-16 23:18:52 -0400

    component template for adjusting settings

---
+++ src/components/SettingsPanel.tsx
@@ -0,0 +1,17 @@
+import "./SettingsPanel.css";
+
+export const SettingsPanel = () => {
+    return (
+        <div id="body">
+            <div className="item">
+                INPUT1<input></input>
+            </div>
+            <div>
+                INPUT2<input></input>
+            </div>
+            <div>
+                INPUT3<input></input>
+            </div>
+        </div>
+    );
+};
