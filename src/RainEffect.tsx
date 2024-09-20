893b16c4ed0400109862997ae64c81ef2582da52
unknown <jyfan16@hotmail.com>
2024-09-20 00:13:31 -0400

    remove css reference

--- src/RainEffect.tsx
+++ src/RainEffect.tsx
@@ -1,5 +1,4 @@
 import React, { useRef } from "react";
-import "./RainEffect.css";
 import { createRainState, RainEffectState } from "./RainState";
 import { useRainEffect } from "./useRainEffect";
 
