cb855c9f55d8ebaa2539dd4de95c4c5be9c9e36e
unknown <jyfan16@hotmail.com>
2024-09-22 15:54:55 -0400

    export

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { RainEffect } from './RainEffect';
 export { useRainEffect } from './useRainEffect';
+export { createRainState } from './RainState';
+export { type RainEffectState } from './RainState';
